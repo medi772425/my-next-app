cbf5e9e2f9f1538b8bc15237d690a1cc25875e89
medi <medi772425@gmail.com>
2023-01-22 12:33:08 +0900

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+  <Head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"/>
+  </Head>
+  <h1 className="bg-primary text-white p-3">Sample app</h1>
+  <Component {...pageProps} />
+  <footer className="fixed-bottom p-2">
+    <hr className="m-1" />
+    <p className="text-center m-0 p-0">
+      copyright 2022 SYODA-Tuyano.
+    </p>
+  </footer>
+</div>
 }
