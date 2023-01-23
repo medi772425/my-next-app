a5e9b5891af2cf64dfb4b8c15694326c2a941db3
medi <medi772425@gmail.com>
2023-01-24 06:52:39 +0900

    4-3 APIを作成する

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,17 @@
-import Head from 'next/head'
 import '../styles/globals.css'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import 'bootstrap/dist/css/bootstrap.css' //☆
 
 function MyApp({ Component, pageProps }) {
   pageProps.appname = "My Next App"
   pageProps.author = "SYODA-Tuyano"
-  const getPage = Component.getPage || ((page) => page) //☆
+  const getPage = Component.getPage || ((page) => page)
   return getPage(<div>
-    <Head>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"/>
-    </Head>
     <Header appname={pageProps.appname} />
     <Component {...pageProps} />
     <Footer author={pageProps.author}/>
   </div>)
 }
 
-
 export default MyApp
-
